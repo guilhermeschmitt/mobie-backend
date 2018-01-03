331a9763e89959d4a7f0140e8eebbf368b41c13f
jhonata11 <jhonata11@hotmail.com>
2018-01-03 21:57:42 -0200

    removing book api from dotenv

--- src/services/BookService.js
+++ src/services/BookService.js
@@ -2,6 +2,7 @@ import Service from './Service'
 import qs from 'query-string'
 import request from 'request-promise'
 
+const BOOK_URI="https://www.googleapis.com/books/v1/volumes" 
 export class BookService extends Service {
 
   _convertToBook(parsed) {
@@ -21,14 +22,14 @@ export class BookService extends Service {
   }
 
   async findById(id) {
-    const json = await request.get(`${process.env.BOOKS_API_URI}/${id}`)
+    const json = await request.get(`${BOOK_URI}/${id}`)
     const parsed = JSON.parse(json)
     return this._convertToBook(parsed)
   }
 
   async searchBook(query, { limit = 20, offset = 0 } = {}) {
     const args = { q: query, startIndex: offset, maxResults: limit, printType: 'books', projection: 'lite' }
-    const json = await request.get(`${process.env.BOOKS_API_URI}?${qs.stringify(args)}`)
+    const json = await request.get(`${BOOK_URI}?${qs.stringify(args)}`)
     const parsed = JSON.parse(json)
     return {
       totalItems: parsed.totalItems,
