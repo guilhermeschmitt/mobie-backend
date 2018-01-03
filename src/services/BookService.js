428749081a81799c5e6c0a9a62927818db16b769
jhonata11 <jhonata11@hotmail.com>
2018-01-03 00:47:40 -0200

    0001: changing book api to google and makings some tests

--- src/services/BookService.js
+++ src/services/BookService.js
@@ -1,39 +1,51 @@
 import Service from './Service'
-
+import qs from 'query-string'
 import request from 'request-promise'
 import { Book } from '../models'
 
-const rp = request.defaults({ 'proxy': 'http://192.168.0.254:3128' })
 export class BookService extends Service {
   constructor(db) {
     super(db)
     this.bookRepo = this.db.getRepository(Book)
   }
 
-  async save(book) {
-    return this.bookRepo.save(book)
+  convertToBook(parsed) {
+    return {
+      id: parsed.id,
+      title: parsed.volumeInfo.title,
+      subtitle: parsed.volumeInfo.subtitle,
+      authors: parsed.volumeInfo.authors,
+      publishedDate: parsed.volumeInfo.publishedDate,
+      description: parsed.volumeInfo.description,
+      pageCount: parsed.volumeInfo.pageCount,
+      categories: parsed.volumeInfo.categories,
+      industryIdentifiers: parsed.volumeInfo.industryIdentifiers,
+      imageLinks: parsed.volumeInfo.imageLinks,
+      language: parsed.volumeInfo.language
+    }
   }
 
   async findById(id) {
-    return this.bookRepo.findOneById(id, { relations: ["authors", "genre"] })
+    const json = await request.get(`https://www.googleapis.com/books/v1/volumes/${id}`)
+    const parsed = JSON.parse(json)
+    return this.convertToBook(parsed)
   }
 
-  async searchBook(attr, { limit = 50, offset = 0 } = {}) {
-    const query = Object.getOwnPropertyNames(attr).length > 0 ? "LOWER(book.title) LIKE LOWER(:title) OR book.isbn LIKE :isbn" : ''
-    return this.bookRepo.createQueryBuilder("book")
-      .where(query)
-      .take(limit)
-      .skip(offset)
-      .orderBy("book.title", "ASC")
-      .setParameters({ title: `%${attr.title}%`, isbn: `%${attr.isbn}%` })
-      .getMany()
+  async searchBook(query, { limit = 20, offset = 0 } = {}) {
+    const args = { q: query, startIndex: offset, maxResults: limit, printType: 'books' }
+    const json = await request.get(`https://www.googleapis.com/books/v1/volumes?${qs.stringify(args)}`)
+    const parsed = JSON.parse(json)
+    return {
+      totalItems: parsed.totalItems,
+      items: parsed.items.map(el => this.convertToBook(el))
+    }
   }
 
-  async teste(){
-    return rp('https://www.googleapis.com/books/v1/volumes?q=the%20gunslinger')
-    .then(function (a) {
-       return a
-    })
+  async teste() {
+    return request('https://www.googleapis.com/books/v1/volumes?q=the%20gunslinger')
+      .then(function (a) {
+        return a
+      })
   }
 
 }
--- test/services/BookService.spec.js
+++ test/services/BookService.spec.js
@@ -4,11 +4,9 @@ import { createDb } from '../IntegrationTest'
 import { BookService } from '../../src/services/BookService'
 
 describe('BookService', function () {
+  this.timeout(3000)
   let conn = undefined
   let bookService = undefined
-
-  const book1 = { title: 'The Gunslinger', release: new Date(), isbn: '111' }
-  const book2 = { title: 'It', release: new Date(), isbn: '222' }
 
   beforeEach(async () => {
     conn = await createDb(this)
@@ -19,96 +17,46 @@ describe('BookService', function () {
     await conn.close()
   })
 
-  it('book => should save', async () => {
-    const savedBook = await bookService.save(book1)
-    should.exists(savedBook.id)
-  })
-
   it('find => by id', async () => {
-    const saved = await bookService.save(book1)
-    const found = await bookService.findById(saved.id)
-    found.should.have.property('id').which.is.a.Number()
-    should.equal(found.title, 'The Gunslinger')
-    should.equal(found.isbn, '111')
+    const found = await bookService.findById('xeIoDwAAQBAJ')
+    should.equal(found.id, 'xeIoDwAAQBAJ')
+    should.equal(found.title, 'The Dark Tower I (MTI)')
+    should.equal(found.subtitle, 'The Gunslinger')
+    should.equal(found.authors[0], 'Stephen King')
+    should.equal(found.publishedDate, "2017-06-27")
+    should.equal(found.pageCount, 384)
+    should.equal(found.categories.length, 5)
+    should.equal(found.industryIdentifiers[0].identifier, '1501166115') 
+    should.equal(found.industryIdentifiers[1].identifier, '9781501166112') 
+    should.equal(found.industryIdentifiers[1].identifier, '9781501166112') 
+    should.equal(found.language, 'en') 
+    found.should.have.property('imageLinks').which.is.a.Object()
+    
   })
 
   it('find => search by title', async () => {
-    await global.Promise.all([bookService.save(book1), bookService.save(book2)])
-    const result = await bookService.searchBook({ title: 'the gunslinger' }, { limit: 1 })
+    const result = await bookService.searchBook('the gunslinger', { limit: 1 })
+    result.should.have.property('totalItems').which.is.a.Number()
 
-    should.equal(result.length, 1)
-    const found = result[0]
-    found.should.have.property('id').which.is.a.Number()
-    should.equal(found.title, 'The Gunslinger')
-    should.equal(found.isbn, '111')
+    const item = result.items[0]
+    should.equal(item.id, 'xeIoDwAAQBAJ')
   })
 
   it('find => search by title partial', async () => {
-    await global.Promise.all([bookService.save(book1), bookService.save(book2)])
-    const result = await bookService.searchBook({ title: 'the guns' }, { limit: 1 })
-
-    should.equal(result.length, 1)
-    const found = result[0]
-    found.should.have.property('id').which.is.a.Number()
-    should.equal(found.title, 'The Gunslinger')
-    should.equal(found.isbn, '111')
-  })
-
-  it('find => search by isbn', async () => {
-    await global.Promise.all([bookService.save(book1), bookService.save(book2)])
-    const result = await bookService.searchBook({ isbn: '111' }, { limit: 1 })
-
-    should.equal(result.length, 1)
-    const found = result[0]
-    found.should.have.property('id').which.is.a.Number()
-    should.equal(found.title, 'The Gunslinger')
-    should.equal(found.isbn, '111')
+    const result = await bookService.searchBook('the lord of the ring', { limit: 1 })
+    result.should.have.property('totalItems').which.is.a.Number()
+    
+    const item = result.items[0]
+    should.equal(item.id, 'yl4dILkcqm4C')
   })
 
   it('find => should limit specified', async () => {
-    await global.Promise.all([bookService.save(book1), bookService.save(book2)])
-    const result = await bookService.searchBook({}, { limit: 1 })
-
-    should.equal(result.length, 1)
-    const found = result[0]
-    found.should.have.property('id').which.is.a.Number()
-    should.equal(found.title, 'It')
-    should.equal(found.isbn, '222')
-  })
-
-  it('find => should return both', async () => {
-    await global.Promise.all([bookService.save(book1), bookService.save(book2)])
-    const result = await bookService.searchBook({}, { limit: 2 })
-    should.equal(result.length, 2)
+    const result = await bookService.searchBook('the gunslinger', { limit: 1 })
+    should.equal(result.items.length, 1)
   })
 
   it('find => should consider the offset', async () => {
-    await global.Promise.all([bookService.save(book1), bookService.save(book2)])
-    const result = await bookService.searchBook({}, { offset: 1 })
-
-    should.equal(result.length, 1)
-    const found = result[0]
-    found.should.have.property('id').which.is.a.Number()
-    should.equal(found.title, 'The Gunslinger')
-    should.equal(found.isbn, '111')
-  })
-
-  it('find => should return zero if wrong arguments', async () => {
-    await bookService.save(book1)
-    const result = await bookService.searchBook({ wrong: 'The Gunslinger' })
-    should.equal(result.length, 0)
-  })
-
-  it('find => should ignore undefined arguments', async () => {
-    await bookService.save(book1)
-    const [found] = await bookService.searchBook({ title: 'The Gunslinger', isbn: undefined })
-    found.should.have.property('id').which.is.a.Number()
-    should.equal(found.title, 'The Gunslinger')
-    should.equal(found.isbn, '111')
-  })
-
-  it.only('find => should ignore undefined arguments', async () => {
-    const a = await bookService.teste()
-    console.log(a)
+    const result = await bookService.searchBook('the gunslinger', { offset: 1 })
+    should.equal(result.items[0].id, 'wOkqDwAAQBAJ')
   })
 })
